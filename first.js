2ef4783f9f4ec8f9733bff0e4ce451f165a3fb6c
sahaabtariq09 <sahaabtariq09@gmail.com>
2023-11-18 22:47:05 +0500

    first

---
+++ first.js
@@ -0,0 +1,8 @@
+let text = prompt("Enter a text to analyze")
+console.log("text.length")
+let splits = prompt(" ")
+console.log("The number of words in the text are")
+let first_name = "Sahaab"
+let last_name = "Tariq"
+let full_name = first_name + last_name
+alert(full_name)
